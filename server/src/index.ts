b5268b30e94c437848dade9f55518ba36d3e73af
sylwia <sylwiavv@gmail.com>
2024-12-22 13:05:00 +0100

    add vercel deploy command

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,6 @@ const app = express()
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.use('/users', require('./routes/userRoute'))
 
