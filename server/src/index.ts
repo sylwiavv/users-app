53b0c043fca019a8784b63f836e1222b520a59a7
sylwia <sylwiavv@gmail.com>
2024-12-25 12:19:33 +0100

    up 12:19

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 9000;
 const path = require('path');
+import  { Request, Response, NextFunction } from "express";
 
 const express = require('express')
 const cors = require('cors')
@@ -16,6 +17,17 @@ app.use('/managers', require('./routes/managerRoute'))
 
 app.use('/signin', require('./routes/signInUserRoute'))
 // ------------------------------
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "https://users-app-frontend-phi.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
 
 app.use(cors({
     origin: ["https://users-app-frontend-phi.vercel.app"],
