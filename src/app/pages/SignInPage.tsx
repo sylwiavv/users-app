1364496342f422d7c1162bb99b3e191f5e1fccbb
sylwia <sylwiavv@gmail.com>
2025-01-02 02:28:38 +0100

    02:28

--- src/app/pages/SignInPage.tsx
+++ src/app/pages/SignInPage.tsx
@@ -1,6 +1,8 @@
 import { SignInForm } from "../components/organizms/forms/SignIn/SignInForm";
 
 const SignInPage = () => {
+  const IS_PRODUCTION = process.env.NODE_ENV === "production";
+console.log(IS_PRODUCTION, "IS_PRODUCTION")
   return (
     <div className="sign__form in">
       <h2 className="sign__title">Sign In</h2>
